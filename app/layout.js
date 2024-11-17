a241a6ac48cba3ecdb1eb19b00865d6f1692904c
brian <briansh1225@gmail.com>
2024-11-18 00:59:49 +0800

    added favicons

--- app/layout.js
+++ app/layout.js
@@ -23,6 +23,15 @@ const bodyFont = Work_Sans({
 export const metadata = {
   title: "Double L Villa",
   description: "Where You Live to Love and Laugh",
+  icons: {
+    icon: [
+      { rel: "icon", type: "image/png", sizes: "16x16", url: "/favicon-16x16.png" },
+      { rel: "icon", type: "image/png", sizes: "32x32", url: "/favicon-32x32.png" },
+    ],
+    apple: "/apple-touch-icon.png",
+    shortcut: "/favicon.ico",
+  },
+  manifest: "/site.webmanifest", // Optional: If you have a web app manifest
 };
 
 export default function RootLayout({ children }) {

900495316250bfcadabe51d24739e1fe34c967cf
brianooi <briansh1225@gmail.com>
2025-05-13 20:27:14 +0800

    removed the head element

--- app/layout.js
+++ app/layout.js
@@ -38,12 +38,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        {/* Favicon links */}
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="manifest" href="/site.webmanifest" />
+      
       </Head>
       <body
         className={`${headingFont.variable} ${bodyFont.variable} antialiased`}

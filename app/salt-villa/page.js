0ee5a157c9064dc00261b817915fb1ed64d68d56
brianooi <briansh1225@gmail.com>
2025-05-13 20:18:56 +0800

    change head

--- app/salt-villa/page.js
+++ app/salt-villa/page.js
@@ -7,93 +7,86 @@ import { useState } from 'react'
 
 
 const rooms = [
-     {title: "Rooftop Pool",
+  {
+    title: "Rooftop Pool",
     bed: "",
     roomImages: [
       "/villa-images/salt-villa/salt-villa-rooftop-1.jpg",
-       "/villa-images/salt-villa/salt-villa-rooftop-2.jpg",
-         "/villa-images/salt-villa/salt-villa-rooftop-3.jpg",
-      
+      "/villa-images/salt-villa/salt-villa-rooftop-2.jpg",
+      "/villa-images/salt-villa/salt-villa-rooftop-3.jpg",
+
 
     ]
   },
-  {title: "Bedroom 1",
+  {
+    title: "Bedroom 1",
     bed: 'King',
     additions: '2 Single Floor Mattress',
     roomImages: [
-      '/villa-images/salt-villa/salt_villa_mb_1.jpg',
-      '/villa-images/salt-villa/salt_villa_mb-2.jpg',
-      '/villa-images/salt-villa/salt_villa_mb-3.jpg',
+      "/villa-images/salt-villa/salt_villa_mb_1.jpg",
+      "/villa-images/salt-villa/salt_villa_mb-2.jpg",
+      "/villa-images/salt-villa/salt_villa_mb-3.jpg",
 
     ]
   },
 
-   {title: "Bedroom 2",
+  {
+    title: "Bedroom 2",
     bed: 'Queen',
-    additions:'Single Floor Mattress',
+    additions: 'Single Floor Mattress',
     roomImages: [
-      '/villa-images/salt-villa/salt_villa_qb-2.jpg',
-      '/villa-images/salt-villa/salt_villa_qb-3.jpg',
-       '/villa-images/salt-villa/salt_villa_qb-1.jpg',
+      "/villa-images/salt-villa/salt_villa_qb-2.jpg",
+      "/villa-images/salt-villa/salt_villa_qb-3.jpg",
+      "/villa-images/salt-villa/salt_villa_qb-1.jpg",
 
     ]
   },
 
-   {title: "Living Room",
+  {
+    title: "Living Room",
     bed: "",
     roomImages: [
       "/villa-images/salt-villa/salt_villa_living_room_1.jpg",
-       "/villa-images/salt-villa/salt_villa_living_room_2.jpg",
-              '/villa-images/salt-villa/salt_villa_5.jpg',
-
-        // "/villa-images/salt-villa/salt_villa_living_room_3.jpg"
-      
-
+      "/villa-images/salt-villa/salt_villa_living_room_2.jpg",
+      "/villa-images/salt-villa/salt_villa_5.jpg",
     ]
   },
 
-  
-
-
 ]
 
-const features=[
-  { 
+const features = [
+  {
     title: 'Picturesque Exterior',
     description: 'Salt Villa draws inspiration from Mediterranean and Balinese architectural styles, every corner is Instagram-perfect.',
-    featureImage:'/villa-images/salt-villa/salt-villa-exterior.jpg'
+    featureImage: '/villa-images/salt-villa/salt-villa-exterior.jpg'
   }
   ,
-   { 
+  {
     title: 'Shower Room',
     description: 'All shower rooms feature a sleek, modern design and provide Malaysia-inspired botanical bath amenities and rain showerheads  for a refreshing, relaxing experience.',
-    featureImage:'/villa-images/salt-villa/salt_villa_shower.jpg'
+    featureImage: '/villa-images/salt-villa/salt_villa_shower.jpg'
   },
-  { 
+  {
     title: 'Kitchen',
     description: 'Salt Villa includes a fully equipped kitchen with modern appliances and a clean design that blends seamlessly with the villa’s minimalist aesthetic',
-    featureImage:'/villa-images/salt-villa/salt-villa-kitchen.jpg'
+    featureImage: '/villa-images/salt-villa/salt-villa-kitchen.jpg'
   },
 
-  { 
-   
-    featureImage:'/villa-images/salt-villa/salt-villa-outdoors.jpg'
+  {
+
+    featureImage: '/villa-images/salt-villa/salt-villa-outdoors.jpg'
   },
 
 
-  { 
-   
-    featureImage:'/villa-images/salt-villa/salt-villa-outdoors-2.jpg'
+  {
+
+    featureImage: '/villa-images/salt-villa/salt-villa-outdoors-2.jpg'
   },
 
-   { 
-   
-    featureImage:"/villa-images/salt-villa/salt_villa_living_room_3.jpg"
+  {
+
+    featureImage: "/villa-images/salt-villa/salt_villa_living_room_3.jpg"
   }
-
-
-
- 
 
 ]
 
@@ -112,9 +105,9 @@ const InfoItem = ({ icon, label, type }) => (
 
 function SaltVilla() {
 
-   const [showFullList, setShowFullList] = useState(false);
+  const [showFullList, setShowFullList] = useState(false);
 
-    const minAmenitiesCount = 5;
+  const minAmenitiesCount = 5;
 
   const displayedAmenities = showFullList ? amenities : amenities.slice(0, Math.min(minAmenitiesCount, amenities.length));
 
@@ -126,45 +119,45 @@ function SaltVilla() {
   return (
     <main>
 
-    <section className='min-h-[70vh] grid place-items-center bg-cover bg-center bg-salt-hero'>
+      <section className='min-h-[70vh] grid place-items-center bg-cover bg-center bg-salt-hero'>
 
-    {/* <div>
+        {/* <div>
             <h1 className='text-6xl font-headingFont text-black text-center '>Salt Villa</h1>
 
         <h2 className='text-3xl font-headingFont text-black text-center'>A pinch of charm in every stay.</h2>
 
     </div> */}
 
-    
-
-    </section>
 
 
- <section className="villa-slider-section  bg-gradient-to-b from-white to-llbeige py-10 md:py-14 xl:py-20 px-5">
-
-    <div className='max-w-screen-lg m-auto'>
-       <div className="border-solid border-[1px] gap-5 border-black flex justify-center rounded-full items-center mb-5  w-fit py-1 px-8">
-              <Image alt="star" width={31} height={31} src="/star-icon.svg"/>
-              <h3 className="font-headingFont sm:text-2xl">Salt Villa</h3>
-            </div>
-
-    <div className='sm:flex sm:gap-10 sm:justify-between'>
-
-    <h1 className=' text-4xl md:text-6xl basis-1/2 max-w-[600px] mb-5 font-headingFont font-medium italic text-llbrown'>A natural retreat to unwind, breathe, and embrace the calm of life.</h1>
-
-    <p className='basis-1/2 max-w-[300px] font-bodyFont mb-5 text-llblack'>Our spacious 2-bedroom villa, which can accommodate up to 8 guests, features a rooftop pool, perfect for a weekend getaway. </p>
+      </section>
 
 
+      <section className="villa-slider-section  bg-gradient-to-b from-white to-llbeige py-10 md:py-14 xl:py-20 px-5">
 
-    </div>
+        <div className='max-w-screen-lg m-auto'>
+          <div className="border-solid border-[1px] gap-5 border-black flex justify-center rounded-full items-center mb-5  w-fit py-1 px-8">
+            <Image alt="star" width={31} height={31} src="/star-icon.svg" />
+            <h3 className="font-headingFont sm:text-2xl">Salt Villa</h3>
+          </div>
 
-    <div id="amenities" className='grid gap-6 sm:justify-items-center sm:my-20 grid-cols-2 sm:grid-cols-5'>
+          <div className='sm:flex sm:gap-10 sm:justify-between'>
 
-   {displayedAmenities.map((amenity, index) => (
+            <h1 className=' text-4xl md:text-6xl basis-1/2 max-w-[600px] mb-5 font-headingFont font-medium italic text-llbrown'>A natural retreat to unwind, breathe, and embrace the calm of life.</h1>
+
+            <p className='basis-1/2 max-w-[300px] font-bodyFont mb-5 text-llblack'>Our spacious 2-bedroom villa, which can accommodate up to 8 guests, features a rooftop pool, perfect for a weekend getaway. </p>
+
+
+
+          </div>
+
+          <div id="amenities" className='grid gap-6 sm:justify-items-center sm:my-20 grid-cols-2 sm:grid-cols-5'>
+
+            {displayedAmenities.map((amenity, index) => (
               <InfoItem type={amenity.type} key={index} icon={amenity.icon} label={amenity.title} />
             ))}
-    </div>
-     <div className='text-center mt-5'>
+          </div>
+          <div className='text-center mt-5'>
             <button
               className='bg-llbrown text-white font-bodyFont py-2 px-5 rounded-full'
               onClick={() => setShowFullList(!showFullList)}
@@ -174,70 +167,70 @@ function SaltVilla() {
           </div>
 
 
-    </div>
-   
-
-
-    </section>
-
-
-    <section className='px-5 max-w-screen-xl m-auto'>
-
-    {rooms.map((room)=>(
-      <div className='room-row py-5 sm:py-10' key={room.title}>
-      
-      <h2 className='font-headingFont text-3xl text-llbrown mb-5'>{room.title} {room.bed ? ` • ${room.bed} bed` : ''} {room.additions? `• ${room.additions}` : ''}</h2>
-
-      <div className='sm:grid gap-5 sm:grid-cols-3'>
-         {room.roomImages.map((roomImage,index)=>(
-      <Image className='object-cover mb-5 h-full object-center' key={`room-${index}`} alt='room-image' width={500} height={300} src={roomImage}/>
-     ))}
-      </div>
-
-    
-      
-   </div>
-    ))}
-      
-    </section>
-
-    <section className='features-section max-w-screen-lg m-auto px-5 sm:flex sm:justify-around sm:gap-10 sm:flex-wrap'>
-
-    {
-      features.map((feature,index)=>(
-        <div className='feature-card pt-10 md:pt-14 xl:pt-20 max-w-[400px] mb-8' key={index}>
-
-        <Image alt={feature.title} width={500} height={500} className='mb-5' src={feature.featureImage}/>
-
-        <h2 className='font-headingFont text-llbrown text-3xl md:text-4xl mb-4 mr-5'>
-          {feature.title}
-        </h2>
-
-        <p className='font-bodyFont text-llblack text-justify mr-10'>{feature.description}</p>
-
-
         </div>
-      ))
-    }
 
 
 
-    </section>
+      </section>
 
-    <section>
-    <div className='max-w-screen-xl m-auto'>
-       <div className="border-solid border-[1px] gap-5 border-black flex justify-center rounded-full items-center mb-5  w-fit py-1 px-8">
-              <Image alt="star" width={31} height={31} src="/star-icon.svg"/>
-              <h3 className="font-headingFont sm:text-2xl">We&apos;re Here To Help</h3>
+
+      <section className='px-5 max-w-screen-xl m-auto'>
+
+        {rooms.map((room) => (
+          <div className='room-row py-5 sm:py-10' key={room.title}>
+
+            <h2 className='font-headingFont text-3xl text-llbrown mb-5'>{room.title} {room.bed ? ` • ${room.bed} bed` : ''} {room.additions ? `• ${room.additions}` : ''}</h2>
+
+            <div className='sm:grid gap-5 sm:grid-cols-3'>
+              {room.roomImages.map((roomImage, index) => (
+                <Image className='object-cover mb-5 h-full object-center' key={`room-${index}`} alt='room-image' width={500} height={300} src={roomImage} />
+              ))}
             </div>
-            <h1 className='text-6xl text-llbrown italic font-headingFont'>
-              Your Questions,<br />
-Perfectly Answered.
-            </h1>
-            <FAQ/>
-    </div>
-      
-    </section>
+
+
+
+          </div>
+        ))}
+
+      </section>
+
+      <section className='features-section max-w-screen-lg m-auto px-5 sm:flex sm:justify-around sm:gap-10 sm:flex-wrap'>
+
+        {
+          features.map((feature, index) => (
+            <div className='feature-card pt-10 md:pt-14 xl:pt-20 max-w-[400px] mb-8' key={index}>
+
+              <Image alt={feature.title} width={500} height={500} className='mb-5' src={feature.featureImage} />
+
+              <h2 className='font-headingFont text-llbrown text-3xl md:text-4xl mb-4 mr-5'>
+                {feature.title}
+              </h2>
+
+              <p className='font-bodyFont text-llblack text-justify mr-10'>{feature.description}</p>
+
+
+            </div>
+          ))
+        }
+
+
+
+      </section>
+
+      <section>
+        <div className='max-w-screen-xl m-auto'>
+          <div className="border-solid border-[1px] gap-5 border-black flex justify-center rounded-full items-center mb-5  w-fit py-1 px-8">
+            <Image alt="star" width={31} height={31} src="/star-icon.svg" />
+            <h3 className="font-headingFont sm:text-2xl">We&apos;re Here To Help</h3>
+          </div>
+          <h1 className='text-6xl text-llbrown italic font-headingFont'>
+            Your Questions,<br />
+            Perfectly Answered.
+          </h1>
+          <FAQ />
+        </div>
+
+      </section>
 
 
     </main>
